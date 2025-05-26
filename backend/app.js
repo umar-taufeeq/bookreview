0255b551ca1a5638b08bc99403d17488621a4806
umar-taufeeq <umarckp111@gmail.com>
2025-05-26 18:00:34 +0530

    refactor: Update CORS configuration to allow specific origins

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,26 @@ const userRoutes = require('./routes/userRoutes');
 //  const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://bookreview-frontend-yjpw.onrender.com'
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 
-app.use(cors ({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  }));
+// app.use(cors ({
+//     origin: 'http://localhost:5173',
+//     'https://bookreview-frontend-yjpw.onrender.com'
+//     credentials: true,
+//   }));
 app.use(express.json()); // parse JSON
 app.use(cookieparser());
 app.use(express.urlencoded({extended: true}));// parse URL-encoded data
