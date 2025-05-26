ead17eecf3f113eb46aaa2dfcfc3e2cef39144b2
Taufeeq Ahmed <168508052+umar-taufeeq@users.noreply.github.com>
2025-05-26 17:49:49 +0530

    Update Axios.jsx

--- frontend/src/utils/Axios.jsx
+++ frontend/src/utils/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/', // Adjust this if your backend runs on a different port
+  baseURL: 'https://bookreview-j8bq.onrender.com/', // Adjust this if your backend runs on a different port
   withCredentials: true, // important for sending cookies (JWT)
 });
 
